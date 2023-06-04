975d076874fcafbad18860d64b93c8891e22f952
Gevorg <mirzoyan.gevorg.23@gmail.com>
2023-06-04 19:55:17 +0400

    Login and Register

---
+++ js/helpers.js
@@ -0,0 +1,21 @@
+const bar = document.querySelector('.fa-bars')
+const clBar = document.querySelector('.fa-xmark')
+const infoStructure = document.querySelector('.bottomList')
+const registerr = document.getElementById('register')
+const inputContainer = document.getElementById('input-container')
+const imgSection = document.querySelector('.img_section')
+const barLogin = document.querySelector('.btn2')
+const login  = document.getElementById('login')
+const loginContainers = document.getElementById('login-container')
+
+export {
+    bar,
+    clBar,
+    infoStructure,
+    registerr,
+    inputContainer,
+    imgSection,
+    barLogin,
+    login,
+    loginContainers
+}
